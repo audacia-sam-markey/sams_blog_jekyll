1c9b6a11823ce377696d007ab2807078edc838c8
audacia-sam-markey <sam.markey@audacia.co.uk>
2022-11-24 16:34:43 +0000

    feat: fixed serverless function

--- netlify/functions/handler.js
+++ netlify/functions/handler.js
@@ -1,20 +1,24 @@
-const API_ENDPOINT = "https://api.gridfox.com/data/Subscribers";
-export async function subscribeToMailingList(event, context) {
-  const fetch = require("node-fetch");
+import fetch from "node-fetch";
+exports.handler = async function (event, context) {
+  const API_ENDPOINT = "https://api.gridfox.com/data/Subscribers";
+
   // Only allow POST
   if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed" };
   }
-
-  const params = querystring.parse(event.body);
+  const params = JSON.parse(event.body);
   const data = {
     Email: params.email,
   };
+  console.log("****************************************************");
+  console.log(JSON.stringify(data));
+  console.log("****************************************************");
   return fetch(API_ENDPOINT, {
     headers: {
       Accept: "application/json",
       "gridfox-api-key":
         "Blog.vGeMqiQu2GtwEVgWOSe9dcjSnMEV4n7xFd7yfyQWBWC6d5HmBFDyyTqM2Fi1prMV",
+      "Content-type": "application/json",
     },
     method: "POST",
     body: JSON.stringify(data),
@@ -25,4 +29,4 @@ export async function subscribeToMailingList(event, context) {
       body: JSON.stringify(data),
     }))
     .catch((error) => ({ statusCode: 422, body: String(error) }));
-}
+};
